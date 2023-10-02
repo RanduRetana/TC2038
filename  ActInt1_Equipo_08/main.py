247d054ec6b3ee6ef3e1a92e0b7ecec39390f04d
Randu Retana <randu2002@hotmail.com>
2023-10-01 23:21:42 -0600

    comments

---  ActInt1_Equipo_08/main.py
+++  ActInt1_Equipo_08/main.py
@@ -1,4 +1,13 @@
 # ========================== FUNCIONES AUXILIARES ==========================
+
+# ======================================================================
+# Función que construye la tabla de prefijos para KMP
+#
+# @param pattern: El patrón para el que se construye la tabla.
+# @return: Una lista que contiene la tabla de prefijos.
+# Complejidad: O(n), donde n es la longitud del patrón.
+# ======================================================================
+
 def build_kmp_table(pattern):
     table = [0] * len(pattern)
     j = 0
@@ -9,6 +18,16 @@ def build_kmp_table(pattern):
             j += 1
         table[i] = j
     return table
+
+# ======================================================================
+# Función que realiza una búsqueda de patrón utilizando KMP
+#
+# @param pattern: El patrón que se busca en el texto.
+# @param text: El texto en el que se realiza la búsqueda.
+# @return: Una lista de índices donde se encontró el patrón en el texto.
+# Complejidad: O(m + n), donde m es la longitud del patrón y n es la longitud del texto.
+# ======================================================================
+
 
 def kmp_search(pattern, text):
     indices = []
@@ -25,6 +44,15 @@ def kmp_search(pattern, text):
     return indices
 
 # ========================== PARTE 1: Búsqueda de Códigos Maliciosos ==========================
+
+# ======================================================================
+# Función que busca códigos maliciosos en transmisiones.
+#
+# @return: Una lista de las transmisiones procesadas.
+# Complejidad: O(n * m), donde n es la longitud total de las transmisiones y m es la longitud total de los códigos maliciosos.
+# ======================================================================
+
+
 def search_mcodes_in_transmissions():
     transmissions = [open(f"transmission{i}.txt", "r").read() for i in range(1, 3)]
     mcodes = [open(f"mcode{i}.txt", "r").read() for i in range(1, 4)]
@@ -41,6 +69,15 @@ def search_mcodes_in_transmissions():
     return transmissions
 
 # ========================== PARTE 2: Palíndromos más largos ==========================
+
+# ======================================================================
+# Función que busca el palíndromo más largo en una transmisión.
+#
+# @param transmission: La transmisión en la que se busca el palíndromo.
+# @return: None
+# Complejidad: O(n^3), donde n es la longitud de la transmisión.
+# ======================================================================
+
 def mirroredSubStringSearch(transmission):
     longestMirrored = ""
     for i in range(len(transmission)):
@@ -51,6 +88,15 @@ def mirroredSubStringSearch(transmission):
     print(longestMirrored, transmission.find(longestMirrored), transmission.find(longestMirrored) + len(longestMirrored) - 1)
 
 # ========================== PARTE 3: Substring común más largo ==========================
+
+# ======================================================================
+# Función que encuentra el substring común más largo entre dos transmisiones.
+#
+# @param transmission1: La primera transmisión.
+# @param transmission2: La segunda transmisión.
+# @return: None
+# Complejidad: O(m * n), donde m es la longitud de transmission1 y n es la longitud de transmission2.
+
 def longest_common_substring(transmission1, transmission2):
    
     m, n = len(transmission1), len(transmission2)
@@ -78,7 +124,7 @@ def longest_common_substring(transmission1, transmission2):
     print("Positions in transmission1:", start + 1, start + length)  
 
 # ========================== FUNCIÓN PRINCIPAL ==========================
-def __main__():
+def _main_():
 
     print("\n---------------")
     print("    PARTE 1")
@@ -100,4 +146,4 @@ def __main__():
 
 
 if __name__ == "__main__":
-    __main__()
+    _main_()
