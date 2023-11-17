73aff3756ab67827ba1387cac0177e423777dc18
Marco Randu Retana Vargas <randu2002@hotmailcom>
2023-11-17 13:47:23 -0600

    ejercicio 3

--- ActInt2_Equipo_08/main.py
+++ ActInt2_Equipo_08/main.py
@@ -71,6 +71,48 @@ def floyd_warshall(matriz_distancias):
 # @param matriz_distancias:
 # @return: 
 # ======================================================================
+def ford_fulkerson(matriz_flujos, nodo_inicial, nodo_final):
+    N = len(matriz_flujos)
+    matriz_flujos_residuales = [list(row) for row in matriz_flujos]
+    flujo_maximo = 0
+    padres = bfs(matriz_flujos_residuales, nodo_inicial, nodo_final)
+
+    while padres is not None:
+        flujo_camino = min(matriz_flujos_residuales[padres[i]][i] for i in range(nodo_inicial, nodo_final + 1))
+        flujo_maximo += flujo_camino
+        v = nodo_final
+        while v != nodo_inicial:
+            u = padres[v]
+            matriz_flujos_residuales[u][v] -= flujo_camino
+            matriz_flujos_residuales[v][u] += flujo_camino
+            v = padres[v]
+        padres = bfs(matriz_flujos_residuales, nodo_inicial, nodo_final)
+
+    return flujo_maximo
+
+# ======================================================================
+# Paso 3
+#
+# @param matriz_distancias:
+# @return: 
+# ======================================================================
+def bfs(matriz_flujos_residuales, nodo_inicial, nodo_final):
+    N = len(matriz_flujos_residuales)
+    visitados = [False] * N
+    cola = [nodo_inicial]
+    visitados[nodo_inicial] = True
+    padres = [-1] * N
+
+    while cola:
+        u = cola.pop(0)
+        for ind, val in enumerate(matriz_flujos_residuales[u]):
+            if not visitados[ind] and val > 0:
+                cola.append(ind)
+                visitados[ind] = True
+                padres[ind] = u
+                if ind == nodo_final:
+                    return padres
+    return None
 
 # ======================================================================
 # Paso 4
@@ -157,6 +199,7 @@ def main():
         # 2. Ruta a seguir por el personal que reparte correspondencia (TSP)
         
         # 3. Valor de flujo máximo de información (Ford-Fulkerson o Edmonds-Karp)
+        flujo_maximo = ford_fulkerson(matriz_flujos, 0, N-1)
         
         # 4. La central más cercana a la nueva contratación
         distancias = encontrar_central_mas_cercana_con_dijkstra(matriz_distancias_ampliada, nueva_central_idx)
@@ -184,8 +227,8 @@ def main():
             # archivo_salida.write(str(ruta_correspondencia) + "\n\n")
             
             # 3. Valor del flujo máximo (cuando esté implementado)
-            # archivo_salida.write("3. Valor de flujo máximo de información:\n")
-            # archivo_salida.write(str(valor_flujo_maximo) + "\n\n")
+            archivo_salida.write("3. Valor de flujo máximo de información:\n")
+            #archivo_salida.write(str(flujo_maximo) + "\n\n")
             
             # 4. 
             central_cercana_coordenadas = centrales[central_cercana_idx]
